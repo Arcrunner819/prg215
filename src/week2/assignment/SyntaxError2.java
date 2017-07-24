bca134064f6b4f53ae63c7551d7dd56fa7689753
fam1n <fam1n@192.168.0.8>
2017-07-24 07:51:51 -0400

    finished version 1 tsubo calculator

---
+++ src/week2/assignment/SyntaxError2.java
@@ -0,0 +1,30 @@
+package week2.assignment;
+import java.util.Scanner;
+
+public class SyntaxError2 {
+
+	public static void main(String[] args) {
+		 
+		Scanner keyboard = new Scanner(System.in);
+		System.out.println("Welcome to the mini calculator\n" +
+						   "You have a choice of the following operations:\n" +
+				            "1 - Multiplication\n" +
+						    "2 - Division\n" +
+				            "3 - Addition" + 
+						    "4 - Subtraction\n" +
+				            "Please enter the number of your selection below.\n");
+		
+		
+		
+		 /*System.out.println("Enter two numbers to multiply.");
+         Scanner keyboard = new Scanner(System.in);
+         int n1 = keyboard.nextInt();
+         int n2 = keyboard.nextInt();
+         int product = n1 * n2;
+         System.out.println("The product is " + product);
+         
+         */
+
+	}
+
+}
---
+++ src/week2/textbook/candyCalculator.java
@@ -0,0 +1,36 @@
+package week2.textbook;
+
+
+import java.util.Scanner;
+
+/** use the Harris-Benedict equation to estimate the number of calories your body needs to maintain your weight
+if you do no exercise. And this is called your basal metabolic rate, or BMR. And here are two different formulas one for males:
+
+BMR = 66+(6.3 weight in pounds) + (12.9 x height in inches) - (6.8 x age in years) 
+ 
+ere is the formula for the number of calories you need to ingest to maintain your weight, and it’s based on your current weight 
+in pounds, height in inches, age in years; and a similar formula for males, but with different numbers, here:
+
+BMR = 655 + (4.3 x weight in pounds) + (4.7 x height in inches) - (4.7 x age in years)
+
+*/
+
+
+public class candyCalculator {
+
+	public static void main(String[] args) {
+		//declare variables
+		double weight;
+		double age;
+		double height;
+		
+		Scanner keyboard = new Scanner(System.in);
+		
+		
+		
+		// Gather informatiom from the user
+		
+
+	}
+
+}
